22c375dd3416ae65fc0855b6b356dc3a11f24df3
Li Xin <444786683@qq.com>
2022-04-19 22:18:29 +0800

    upload

---
+++ TRPO/config.py
@@ -0,0 +1,11 @@
+import torch
+
+env_name = 'CartPole-v1'
+gamma = 0.99
+goal_score = 200
+log_interval = 10
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
+
+
+max_kl = 0.01
